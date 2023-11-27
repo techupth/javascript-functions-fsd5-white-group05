f123bb7664713337c9c8fa58baa538dbd2e5aa24
Astrick <mypiarenoo@gmail.com>
2023-11-28 00:40:14 +0700

    jeffry has been finished function's exercises

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,18 @@
-// Start coding here
-let calculator = {};
+// Exercise #3
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
+
+// Exercise #4
+let calculator = {
+    add: add,
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide
+  };
+
+let addResult = calculator.add(10,20);
+console.log(addResult);
+let divideResult = calculator.divide(3000,10)
+console.log(divideResult);
